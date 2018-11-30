c629ef65bfab40f2f139888d776e6b1878a2a09c
cahyo40 <mdcnugroho@gmail.com>
2018-11-30 23:03:38 +0700

    pass mahasiswa

--- src/components/home/mhs/settingMhs.vue
+++ src/components/home/mhs/settingMhs.vue
@@ -1,12 +1,22 @@
 <template>
   <div>
-    <h1 align="center">Halaman Pengaturan</h1>
-    <br>
-    <v-text-field label="Nim" outline v-model="profil.nim" readonly></v-text-field>
-    <v-text-field label="Nama" outline v-model="profil.nama" readonly></v-text-field>
-    <v-text-field label="Alamat" outline v-model="inputan.alamat"></v-text-field>
-    <v-text-field label="Telepon" outline v-model="inputan.telepon"></v-text-field>
-    <v-btn color="primary" @click="update(profil.nim)">Simpan</v-btn>
+    <div>
+      <h1 align="center">Halaman Pengaturan</h1>
+      <br>
+      <h3 align="right">Profil</h3>
+      <br>
+      <v-text-field label="Nim" outline v-model="profil.nim" readonly></v-text-field>
+      <v-text-field label="Nama" outline v-model="profil.nama" readonly></v-text-field>
+      <v-text-field label="Alamat" outline v-model="inputan.alamat"></v-text-field>
+      <v-text-field label="Telepon" outline v-model="inputan.telepon"></v-text-field>
+      <v-btn color="primary" @click="update(profil.nim)">Simpan</v-btn>
+    </div>
+    <div style="margin-top:10px">
+      <h3 align="right">Kata Sandi</h3>
+      <v-text-field type="password" label="Kata Sandi Lama" outline></v-text-field>
+      <v-text-field type="password" label="Kata Sandi Baru" outline></v-text-field>
+      <v-btn color="primary">Ganti Kata Sandi</v-btn>
+    </div>
   </div>
 </template>
 <script>
